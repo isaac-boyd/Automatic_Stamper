ae9799fc40dcd4007abbbd363271df2160d3e17b
Isaac Boyd <isaacjonathanboyd@gmail.com>
2025-05-03 09:50:03 -0400

    Update stamper.h

--- stamper.h
+++ stamper.h
@@ -1,1 +1,18 @@
+#ifndef stamper_h
+#define stamper_h
+#define press write
+#define pressDown 30 
+#define pullUp 15 
+Bool pressAloud = true;
 
+class stamper {
+public:
+    void begin(int servoPin)
+    void stampHand();
+    void detectHand();
+    
+private:
+   
+};
+
+#endif
