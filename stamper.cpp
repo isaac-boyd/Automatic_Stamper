a6ed63f78b96443b48969c5e93516f65b8ebdc67
Isaac Boyd <isaacjonathanboyd@gmail.com>
2025-05-03 09:55:10 -0400

    Update stamper.cpp

--- stamper.cpp
+++ stamper.cpp
@@ -1,1 +1,24 @@
 
+#include "stamper.h"
+#include <Servo.h>
+Servo servo;
+stamper::stamper() {
+    
+}
+
+    void begin(int servoPin){
+      servo.attach(servoPin);
+    };
+    void stampHand(){
+      if (pressAloud == true)
+      stamp.press(pressDown)
+      delay(500);
+      stamp.press(pullUp)
+      pressAloud = false; 
+      else{
+      stamp.press(pullUp)
+      delay(500);
+      pressAloud = true; 
+      }//All of this is to keep it from double stamping.
+    };
+    void detectHand(){};//do this later.
